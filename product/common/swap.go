ce0a16577c9b20cd7cbd3745eaed8c284bcfff39
taoqun <taoqun8316@163.com>
2023-09-27 00:42:19 +0800

    add

--- product/common/swap.go
+++ product/common/swap.go
@@ -2,10 +2,10 @@ package common
 
 import "encoding/json"
 
-func SwapTo(request, category interface{}) (err error) {
+func SwapTo(request, product interface{}) (err error) {
 	dataByte, err := json.Marshal(request)
 	if err != nil {
 		return
 	}
-	return json.Unmarshal(dataByte, category)
+	return json.Unmarshal(dataByte, product)
 }
--- product/handler/product.go
+++ product/handler/product.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"product/common"
+	"product/domain/model"
 	"product/domain/service"
 	pb "product/proto"
 )
@@ -10,27 +12,64 @@ type Product struct {
 	ProductDataService service.IProductDataService
 }
 
-func (p Product) AddProduct(ctx context.Context, info *pb.ProductInfo, product *pb.ResponseProduct) error {
-	//TODO implement me
-	panic("implement me")
+func (p Product) AddProduct(ctx context.Context, request *pb.ProductInfo, response *pb.ResponseProduct) error {
+	product := &model.Product{}
+	if err := common.SwapTo(request, product); err != nil {
+		return err
+	}
+	id, err := p.ProductDataService.AddProduct(product)
+	if err != nil {
+		return err
+	}
+	response.ProductId = id
+	return nil
 }
 
-func (p Product) FindProductByID(ctx context.Context, id *pb.RequestID, info *pb.ProductInfo) error {
-	//TODO implement me
-	panic("implement me")
+func (p Product) FindProductByID(ctx context.Context, request *pb.RequestID, response *pb.ProductInfo) error {
+	product, err := p.ProductDataService.FindProductByID(request.ProductId)
+	if err != nil {
+		return err
+	}
+	if err = common.SwapTo(product, response); err != nil {
+		return err
+	}
+	return nil
 }
 
-func (p Product) UpdateProduct(ctx context.Context, info *pb.ProductInfo, response *pb.Response) error {
-	//TODO implement me
-	panic("implement me")
+func (p Product) UpdateProduct(ctx context.Context, request *pb.ProductInfo, response *pb.Response) error {
+	product := &model.Product{}
+	if err := common.SwapTo(request, product); err != nil {
+		return err
+	}
+	err := p.ProductDataService.UpdateProduct(product)
+	if err != nil {
+		return err
+	}
+	response.Message = "修改成功"
+	return nil
 }
 
-func (p Product) DeleteProductByID(ctx context.Context, id *pb.RequestID, response *pb.Response) error {
-	//TODO implement me
-	panic("implement me")
+func (p Product) DeleteProductByID(ctx context.Context, request *pb.RequestID, response *pb.Response) error {
+	err := p.ProductDataService.DeleteProduct(request.ProductId)
+	if err != nil {
+		return err
+	}
+	response.Message = "删除成功"
+	return nil
 }
 
-func (p Product) FindAllProduct(ctx context.Context, all *pb.RequestAll, product *pb.AllProduct) error {
-	//TODO implement me
-	panic("implement me")
+func (p Product) FindAllProduct(ctx context.Context, request *pb.RequestAll, response *pb.AllProduct) error {
+	products, err := p.ProductDataService.FindAllProduct()
+	if err != nil {
+		return err
+	}
+
+	for _, v := range products {
+		productInfo := &pb.ProductInfo{}
+		if err = common.SwapTo(v, productInfo); err != nil {
+			return err
+		}
+		response.ProductInfo = append(response.ProductInfo, productInfo)
+	}
+	return nil
 }
